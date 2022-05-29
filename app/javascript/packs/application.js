e42878dce126b5cb733b43f3e2d84534959bbd7a
chiageng <91059092+chiageng@users.noreply.github.com>
2022-05-29 23:11:18 +0800

    Solve bootstrap unable customise issue, design home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@hotwired/turbo-rails"  
+import "./controllers"  
+import * as bootstrap from "bootstrap"  
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
+  return new bootstrap.Popover(popoverTriggerEl)  
+})  
